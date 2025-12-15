de03526a8952dcd3d53bd1f886214eea174d7e9e
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-15 02:51:25 +0000

    css update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
+
+ 
+const StyledApp = styled.main`
+  background-color:orangered;
+  padding:20px;
 `;
 
 function App(){
   return(
-    <div>
-      <H1>The Wild Oasis</H1>
-    </div>
-  )
+    <>
+      <GlobalStyles/>
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+
+        <Heading as="h2">Check in and out</Heading>
+         <Button onClick={() => alert("check in") }>Check In</Button>
+         <Button onClick={() => alert("check out") }>Check Out</Button>
+
+         <Heading as="h3">Form</Heading>
+         <Input type="number" placeholder="Number of guests"/>
+         <Input type="number" placeholder="Number of guests"/>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
