99a9b4472fb0ece02bcf223936498ab181e7f271
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-25 00:24:18 +0000

    Compound Component Cabins

--- src/ui/Modal copy.jsx
+++ src/ui/Modal copy.jsx
@@ -1,9 +1,6 @@
 import styled from "styled-components";
 import { HiXMark } from "react-icons/hi2";
 import { createPortal } from "react-dom";
-import { cloneElement, createContext } from "react";
-import { useState, useContext } from "react";
-
 
 const StyledModal = styled.div`
   position: fixed;
@@ -54,44 +51,18 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
-
-function Modal({children}) {
-  const [openName, setOpenName] = useState('');
-
-  const close = () => setOpenName('');
-  const open = setOpenName;
-
-  return (
-    <ModalContext.Provider value={{openName, open, close}}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-function Open({children, opens:opensWindowName}) {
-  const {open} = useContext(ModalContext);
-  return cloneElement(children, {onClick: () => open(opensWindowName)});
-}
-
-function Window({children, name}) {
-  const {openName, close} = useContext(ModalContext);
-  if(name !== openName) return;
-
+function Modal({children, onClose}) {
   return createPortal(
   <Overlay>  
     <StyledModal>
-      <Button onClick={close}>
+      <Button onClick={onClose}>
         <HiXMark />
       </Button>
-      <div>{cloneElement(children, {onCloseModal: close})}</div>
+      <div>{children}</div>
     </StyledModal>
   </Overlay>,
   document.body
   );
 }
-
-Modal.Open = Open;
-Modal.Window = Window;
 
 export default Modal;
