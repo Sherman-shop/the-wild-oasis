c6439177530a625c502e0650e3c963118ac0d868
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-26 05:23:58 +0000

    Make Filter component reusable

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,15 +1,23 @@
-import styled from "styled-components";
 import Table from "../../ui/Table";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins, error } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || 'all';
   
+  let filteredCabins = cabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount") filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount") filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
   return (
     <Menus>
       <Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
@@ -22,7 +30,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body 
-          data={cabins} 
+          data={filteredCabins} 
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id}/> }
         />
       </Table>
