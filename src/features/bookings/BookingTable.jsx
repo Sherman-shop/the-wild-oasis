9f104662eb986b107c955bafc51e51c0193a558b
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-27 23:10:48 +0000

    Pagination update

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -2,6 +2,7 @@ import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty";
+import Pagination from "../../ui/Pagination";
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
 
@@ -30,6 +31,9 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+        <Table.Footer >
+          <Pagination count={15}/>
+        </Table.Footer>
       </Table>
     </Menus>
   );
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -1,4 +1,8 @@
+import { HiChevronLeft } from "react-icons/hi2";
 import styled from "styled-components";
+import { HiChevronRight } from "react-icons/hi2";
+import { useSearchParams } from "react-router-dom";
+
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -55,3 +59,46 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+const PAGE_SIZE = 10;
+
+function Pagination({count}) {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const currentPage = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+
+  function nextPage() {
+    const next = currentPage === pageCount ? currentPage : currentPage +1;
+    searchParams.set("page", next);
+    setSearchParams(searchParams);
+
+  }
+
+  function previousPage() {
+    const previous = currentPage === 1 ? 1 : currentPage -1;
+    searchParams.set("page", previous);
+    setSearchParams(searchParams);
+  }
+
+  if(pageCount <= 1) return null;
+
+  return (
+    <StyledPagination>
+      <P>
+        showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to <span>{currentPage === pageCount ? count : currentPage * PAGE_SIZE}</span> of <span>{count}</span> results
+      </P>
+
+    <Buttons>
+      <PaginationButton onClick={previousPage} disabled={currentPage === 1}>
+        <HiChevronLeft /><span>Previous</span>
+      </PaginationButton>
+      <PaginationButton onClick={nextPage} disabled={currentPage === pageCount}>
+        <span>Next</span><HiChevronRight />
+      </PaginationButton>
+    </Buttons>  
+    </StyledPagination>
+  )
+}
+  
+export default Pagination;
