772378170f944876a947788f9bfecbc8ac8f4ee7
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-28 00:36:10 +0000

    Fix server-side pagination bug in bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {  
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   //Filter
@@ -14,10 +17,29 @@ export function useBookings() {
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
-  const {isLoading, data:bookings, error} = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+  //Pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
+
+  //Query
+  const {isLoading, data:{ data: bookings, count } = {}, error} = useQuery({
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-    return { isLoading, bookings, error };
+  //Pre-fetching
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+  if(page < pageCount)
+  queryClient.prefetchQuery({
+    queryKey: ["bookings", filter, sortBy, page + 1],
+    queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+  })
+
+  if(page > 1)
+  queryClient.prefetchQuery({
+    queryKey: ["bookings", filter, sortBy, page - 1],
+    queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+  })
+
+    return { isLoading,  bookings, error, count };
 }
--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -2,6 +2,7 @@ import { HiChevronLeft } from "react-icons/hi2";
 import styled from "styled-components";
 import { HiChevronRight } from "react-icons/hi2";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../utils/constants";
 
 
 const StyledPagination = styled.div`
@@ -60,7 +61,7 @@ const PaginationButton = styled.button`
   }
 `;
 
-const PAGE_SIZE = 10;
+
 
 function Pagination({count}) {
   const [searchParams, setSearchParams] = useSearchParams();
