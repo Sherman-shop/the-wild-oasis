1e5be426ee3f6d6d641f4501b3826e59298a31d1
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-29 03:39:39 +0000

    Authorization Protecting Routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -2,9 +2,11 @@ import { useMutation } from "@tanstack/react-query";
 import { login as loginApi  } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
+import { useQueryClient } from "@tanstack/react-query";
 
 
 export function useLogin() {
+    const queryClient = useQueryClient();
     const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
@@ -12,7 +14,7 @@ export function useLogin() {
         email, password
     }),
     onSuccess: (user) => {
-        
+        queryClient.setQueriesData(['user'], user);
         navigate("/dashboard");
     },
     onError:(err) => {
