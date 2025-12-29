d66d0fa4561bd445bfe9600539a371ca8c30e7d0
Sherman-shop <shuguoheng_sgh@163.com>
2025-12-29 05:35:59 +0000

    sign up update

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,19 @@
 import supabase from "./supabase";
 
+export async function signup({ email, password, fullName }) {
+  const { data, error } = await supabase.auth.signUp({
+    email, password, options: {
+      data: {
+        fullName,
+        avatar:"" 
+      },
+    }
+  });
+  if (error) throw new Error(error.message);
+    
+    return data;
+}
+
 export async function login({ email, password }) {
     
 const { data, error } = await supabase.auth.signInWithPassword({
@@ -17,9 +31,12 @@ export async function getCurrentUser() {
 
   const{ data, error } = await supabase.auth.getUser();
 
-  console.log(data)
-
   if (error) throw new Error(error.message);
     
   return data?.user;
+}
+
+export async function logout() {
+  const {error} = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
